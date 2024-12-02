6203156d9bd683136533458578381236c62e2211
PaterAnderson <DariusMue@web.de>
2024-12-02 12:15:45 +0100

    added navbar

---
+++ src/app/landing-page/landing-page.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { NavbarComponent } from './navbar/navbar.component';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-landing-page',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './landing-page.component.html',
+  styleUrl: './landing-page.component.scss'
+})
+export class LandingPageComponent {
+
+}
