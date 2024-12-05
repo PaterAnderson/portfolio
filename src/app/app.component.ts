41ea7f5ad08fae531b7a3e2675db5a19013d9653
PaterAnderson <DariusMue@web.de>
2024-12-05 14:45:02 +0100

    left part info card done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,11 +6,12 @@ import { NavbarComponent } from './landing-page/navbar/navbar.component';
 import { AboutMeComponent } from './about-me/about-me.component';
 import { SkillSetComponent } from './skill-set/skill-set.component';
 import { ProjectsComponent } from './projects/projects.component';
+import { InfoCardComponent } from './projects/info-card/info-card.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, LandingPageComponent, NavbarComponent, AboutMeComponent, SkillSetComponent, ProjectsComponent],
+  imports: [CommonModule, RouterOutlet, LandingPageComponent, NavbarComponent, AboutMeComponent, SkillSetComponent, ProjectsComponent, InfoCardComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
