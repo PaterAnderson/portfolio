8fddea4350546d1daa085ac25ad93e9899fd5daa
PaterAnderson <DariusMue@web.de>
2024-12-05 09:42:15 +0100

    outsourced html to ts

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -1,12 +1,31 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+
+interface Project {
+  title: string;
+  technologies: string[];
+}
 
 @Component({
   selector: 'app-projects',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './projects.component.html',
   styleUrl: './projects.component.scss'
 })
 export class ProjectsComponent {
-  
+  projects: Project[] = [
+    {
+      title: 'Join',
+      technologies: ['Angular', 'TypeScript', 'HTML', 'CSS', 'Firebase']
+    },
+    {
+      title: 'Sharkie',
+      technologies: ['HTML', 'CSS', 'JavaScript']
+    },
+    {
+      title: 'Pokedex',
+      technologies: ['HTML', 'CSS', 'JavaScript']
+    }
+  ];
 }
