1f8d93fdb9456245889b2439fac4b813411d164d
PaterAnderson <DariusMue@web.de>
2024-12-17 16:01:23 +0100

    added php file with email function

--- src/app/main-content/main-content.component.ts
+++ src/app/main-content/main-content.component.ts
@@ -5,11 +5,12 @@ import { ProjectsComponent } from './projects/projects.component';
 import { CommentsComponent } from './comments/comments.component';
 import { ContactMeComponent } from './contact-me/contact-me.component';
 import { LandingPageComponent } from './landing-page/landing-page.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-main-content',
   standalone: true,
-  imports: [ LandingPageComponent ,AboutMeComponent, SkillSetComponent, ProjectsComponent, CommentsComponent, ContactMeComponent],
+  imports: [ CommonModule, LandingPageComponent ,AboutMeComponent, SkillSetComponent, ProjectsComponent, CommentsComponent, ContactMeComponent],
   templateUrl: './main-content.component.html',
   styleUrl: './main-content.component.scss'
 })
